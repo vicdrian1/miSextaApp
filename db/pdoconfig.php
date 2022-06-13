621eff1076a684f9e63508f43f666d000826f83f
vicdrian1 <104452446+vicdrian1@users.noreply.github.com>
2022-06-13 16:50:18 +0200

    Update pdoconfig.php
    Updated data

--- db/pdoconfig.php
+++ db/pdoconfig.php
@@ -1,10 +1,10 @@
 
 <?php
 session_start();
-$host = 'localhost';
-$dbname = 'ceisbook';
-$username = 'fran';
-$password = '1234';
+$host = 'oliadkuxrl9xdugh.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
+$dbname = 'veod2bb44wdxa5x0';
+$username = 'q3yx3slix37u9sm4';
+$password = 'dm7imjjn2z0jm4ne';
 
 $options = [
     \PDO::ATTR_ERRMODE  => \PDO::ERRMODE_EXCEPTION,
